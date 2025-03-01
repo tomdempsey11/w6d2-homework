1fd87cef2582ef16866e84fdf5f88bbd7a123e60
Tom Dempsey <tomdempsey@Mac.lan>
2025-03-01 00:27:43 -0800

    made header responsive, table responsive with bootstrap functionality and contact form responsive

--- w2d2-homework/js/activityselect.js
+++ w2d2-homework/js/activityselect.js
@@ -1,35 +1,37 @@
-//user interaction with table cells
 $(document).ready(function () {
+    let selectedActivities = []; // Array to store selected activities
+
     $("td").click(function () {
-        var content = $(this).text(); // get content of cell
+        var content = $(this).text(); // Get clicked cell content
 
-        if (content != "Not Available") { // check if content is not "Not Available"
-            $(this).toggleClass("tdhighlight"); // toggle highlight class
-
+        if (content !== "Not Available") { // Ignore "Not Available" cells
             // Get the column index of the clicked cell
             var colIndex = $(this).index();
-            // Use the index to get the corresponding header text
+            // Get the corresponding cliff site name
             var cliffSite = $("table thead th").eq(colIndex).text();
+            var activityEntry = content + " at " + cliffSite;
 
-            if ($(this).hasClass("tdhighlight")) { // if the cell is now selected
-                $('#displaySelected').css({
-                    "visibility": "visible",
-                    "margin-top": "2em"
-                });
-                // Append the cell content along with the cliff site name
-                $('#result').append("<p>" + content + " at " + cliffSite + "</p>");
-            } else { // if the cell is deselected
-                // Remove the corresponding paragraph element.
-                // Note: This uses a simple contains() filter so if you have similar content it may need refining.
-                $('#result p:contains(' + content + ')').remove();
+            // Toggle selection (Add/Remove from array)
+            if ($(this).hasClass("tdhighlight")) {
+                // Remove the activity from the selected list
+                selectedActivities = selectedActivities.filter(item => item !== activityEntry);
+                $(this).removeClass("tdhighlight");
+            } else {
+                // Add activity to the list
+                selectedActivities.push(activityEntry);
+                $(this).addClass("tdhighlight");
+            }
 
-                // Hide the display box if there are no selected items
-                if ($('#result').has('p').length == false) {
-                    $('#displaySelected').css({
-                        "visibility": "hidden",
-                        "margin-top": "0"
-                    });
-                }
+            // Update modal content
+            if (selectedActivities.length > 0) {
+                $("#modalContent").html("<strong>Selected Activities:</strong><br>" + selectedActivities.join("<br>"));
+            } else {
+                $("#modalContent").html("<em>No activities selected.</em>");
+            }
+
+            // Open modal only if at least one activity is selected
+            if (selectedActivities.length > 0) {
+                $("#activityModal").modal("show");
             }
         }
     });
